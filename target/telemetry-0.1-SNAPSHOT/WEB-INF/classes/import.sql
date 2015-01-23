428409ee56b2281c75232ac047f934df6a1fde79
Cameron Tuesley <cameron@teamarrow.com.au>
2015-01-23 15:16:16 +1000

    Adsc 2015 changes

--- target/telemetry-0.1-SNAPSHOT/WEB-INF/classes/import.sql
+++
@@ -1,3 +0,0 @@
--- create table public.msrmnt (msrmnt_id bigint not null, data_pnt_can_id integer, msrmnt_name varchar(255), dev_id bigint, dev_type_id bigint, msrmnt_type_id bigint, primary key (msrmnt_id))
-insert into msrmnt values     (1, 1025, 'WSPing', null, null, null)
-insert into msrmnt values     (2, 1035, 'WSTemp', null, null, null)
