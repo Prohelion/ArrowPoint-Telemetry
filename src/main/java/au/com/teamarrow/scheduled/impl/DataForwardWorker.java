ee62a1925c86988fb44f884eb3e0494b0ae2a9d1
Cameron Tuesley <ctuesley@integraltech.com.au>
2018-08-26 23:30:32 +1000

    Now with push functionality

--- src/main/java/au/com/teamarrow/scheduled/impl/DataForwardWorker.java
+++ src/main/java/au/com/teamarrow/scheduled/impl/DataForwardWorker.java
@@ -1,12 +1,23 @@
 package au.com.teamarrow.scheduled.impl;
 
+import static org.hamcrest.CoreMatchers.notNullValue;
+
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 import org.apache.commons.collections4.CollectionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 import au.com.teamarrow.model.DataPoint;
 import au.com.teamarrow.model.MeasurementData;
@@ -15,7 +26,11 @@ import au.com.teamarrow.service.DataPointService;
 import au.com.teamarrow.service.MeasurementDataService;
 
 @Component("dataForwardWorker")
+@PropertySource({"classpath:application.properties"})
 public class DataForwardWorker implements Worker {
+	
+    private static final Logger LOG = LoggerFactory.getLogger(DataForwardWorker.class);
+
 
 	@Autowired
 	DataPointService dataPointService;
@@ -23,18 +38,39 @@ public class DataForwardWorker implements Worker {
 	@Autowired
 	MeasurementDataService measurementDataService;
 	
+    @Autowired
+    private Environment env;
+	
 	@Override
 	public void work() {
 
-		ArrayList<MeasurementData> measurmentData = new ArrayList<MeasurementData>();
+		ArrayList<MeasurementData> measurementData = new ArrayList<MeasurementData>();
 		
 		List<DataPoint> dataPoints = dataPointService.getDataPoints();
 		
 		Iterator<DataPoint> iterator = dataPoints.iterator();
 		
 		while (iterator.hasNext()) {			
-	        CollectionUtils.addAll(measurmentData, measurementDataService.findLatestDataForCanId(iterator.next().getDataPointCanId()));  			
+	        CollectionUtils.addAll(measurementData, measurementDataService.findLatestDataForCanId(iterator.next().getDataPointCanId()));  			
 		}
+		
+		ClientHttpRequestFactory requestFactory = getClientHttpRequestFactory();
+		RestTemplate restTemplate = new RestTemplate(requestFactory);
+		 
+		HttpEntity<ArrayList<MeasurementData>> request = new HttpEntity<>(measurementData);
+		URI location = restTemplate.postForLocation(env.getProperty("data.relay.url"), request);
+		
+		if (location == null)
+			LOG.error("Forwarder has been unable to forward to address (data.relay.url) = " + env.getProperty("data.relay.url") );
+		
+	}
+	
+	private ClientHttpRequestFactory getClientHttpRequestFactory() {
+	    int timeout = 5000;
+	    HttpComponentsClientHttpRequestFactory clientHttpRequestFactory
+	      = new HttpComponentsClientHttpRequestFactory();
+	    clientHttpRequestFactory.setConnectTimeout(timeout);
+	    return clientHttpRequestFactory;
 	}
 
 }
