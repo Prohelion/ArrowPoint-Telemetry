7a018a114da566e565e396729b9d783da566e3d2
Cameron Tuesley <ctuesley@integraltech.com.au>
2018-09-09 18:24:46 +1000

    changes to the login, currently issues with the login form

--- src/main/java/au/com/teamarrow/spring/WebSecurityConfig.java
+++ src/main/java/au/com/teamarrow/spring/WebSecurityConfig.java
@@ -1,7 +1,10 @@
 package au.com.teamarrow.spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,29 +15,39 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
+@PropertySource({"classpath:application.properties"})
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private Environment env;
+	
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/forward-data.json","/car-data.json").permitAll()
+                .antMatchers("/forward-data.json","/car-data.json","/css/**","/img/**","/js/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
                 .loginPage("/login")
                 .permitAll()
+                .defaultSuccessUrl("/index.html", true)
                 .and()
             .logout()
-                .permitAll();
+            	.deleteCookies("JSESSIONID")
+                .permitAll()
+             .and()
+                .rememberMe().key("!uniqueAndSecretKeyThatOnlyWeWouldKnow!");
     }
 
     @Bean
     @Override
     public UserDetailsService userDetailsService() {
-        UserDetails user =
+        @SuppressWarnings("deprecation")
+		UserDetails user =
              User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
+                .username(env.getProperty("telemetry.username"))
+                .password(env.getProperty("telemetry.password"))
                 .roles("USER")
                 .build();
 
