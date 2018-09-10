876dc2b4e8049b1e5daed44d13697c42b7216022
Cameron Tuesley <ctuesley@integraltech.com.au>
2018-09-10 15:02:04 +1000

    Fixed a basic bug

--- docker/splunk/restore_volumes.sh
+++ docker/splunk/restore_volumes.sh
@@ -1,2 +1,2 @@
-docker run --rm --volumes-from teamarrowweb_splunkenterprise_1 -v $(pwd):/backup ubuntu bash -c "cd /backup && wget https://s3.eu-central-1.amazonaws.com/prohelion-splunk-config/etc.tar && cd /opt/splunk/etc && tar xvf /backup/etc.tar --strip 3
-docker run --rm --volumes-from teamarrowweb_splunkenterprise_1 -v $(pwd):/backup ubuntu bash -c "cd /backup && wget https://s3.eu-central-1.amazonaws.com/prohelion-splunk-config/var.tar && cd /opt/splunk/var && tar xvf /backup/var.tar --strip 3
+docker run --rm --volumes-from teamarrowweb_splunkenterprise_1 -v $(pwd):/backup ubuntu bash -c "cd /opt/splunk/etc && tar xvf /backup/etc.tar --strip 3"
+docker run --rm --volumes-from teamarrowweb_splunkenterprise_1 -v $(pwd):/backup ubuntu bash -c "cd /opt/splunk/var && tar xvf /backup/var.tar --strip 3"
